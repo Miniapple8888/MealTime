acd5fb23dfd4b4fc1d9a74a77c1c9edce8debf90
Miniapple8888 <miniapple8888@gmail.com>
2022-03-05 00:58:20 -0500

    Initializing project

---
+++ app/src/main/java/com/example/mealtime/MealItemView.kt
@@ -0,0 +1,33 @@
+package com.example.mealtime
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.widget.ViewPager2
+
+private const val NUM_PAGES = 5
+
+
+class MealItemView: Fragment() {
+    private var mealItemName: String? = null
+    private var param2: String? = null
+    private lateinit var viewPager: ViewPager2
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+        // TODO: Add view pager
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_meal_item, container, false)
+    }
+}
